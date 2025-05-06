e2cdddf31c6fa241dae6e8f21212fda519c4c0bb
JoungYujin <jyjin0205@postech.ac.kr>
2025-05-06 19:48:44 +0900

    Function Without Saving

--- app/src/main/java/com/example/kkubeok/MainActivity.kt
+++ app/src/main/java/com/example/kkubeok/MainActivity.kt
@@ -27,7 +27,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            KkubeokTheme {             KkubeokMain()
+            KkubeokTheme {
+                KkubeokMain()
             }
         }
     }
