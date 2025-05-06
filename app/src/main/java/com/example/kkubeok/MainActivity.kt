f7a2a93f1bf9f40f45366b0a8c884e03480d0e05
glosa <jumi4339@naver.com>
2025-05-06 19:34:52 +0900

    Make Main Scren UI finish

--- app/src/main/java/com/example/kkubeok/MainActivity.kt
+++ app/src/main/java/com/example/kkubeok/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.*
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kkubeok.ui.theme.KkubeokTheme
 
@@ -18,6 +19,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 import com.example.kkubeok.screen.DetectingScreen
 
@@ -49,17 +53,69 @@ fun MainScreen(navController: NavHostController) {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
-                .padding(32.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(horizontal=32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement=Arrangement.Center
         ){
+            // App Name
+            Text(
+                text="\uD83E\uDD71 Kkubeok",
+                style = MaterialTheme.typography.headlineMedium,
+                modifier=Modifier.padding(bottom=32.dp)
+            )
+            // Welcome Comment
+            Text(
+                text= "Hello, What's your name?",
+                style=MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text="Enter Your Name",
+                style=MaterialTheme.typography.bodySmall,
+                color=Color.Gray,
+                modifier=Modifier.padding(bottom=8.dp)
+            )
+            // Name Input Bar
+            var name by remember{mutableStateOf("")}
+            OutlinedTextField(
+                value=name,
+                onValueChange={name=it},
+                placeholder={Text("Gildong")},
+                modifier=Modifier
+                    .fillMaxWidth()
+                    .padding(bottom=24.dp)
+            )
+            // Start Button
+            Button(
+                onClick={/* TODO: Start Click Activity*/},
+                modifier=Modifier
+                    .fillMaxWidth()
+                    .padding(vertical=4.dp),
+                colors=ButtonDefaults.buttonColors(containerColor = Color.Black )
+            ){
+                Text("Start", color=Color.White)
+            }
+            // Detecting Button
             Button(
                 onClick = {navController.navigate("detecting")},
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(vertical = 8.dp)
+                    .padding(vertical = 4.dp),
+                colors=ButtonDefaults.buttonColors(containerColor=Color.Black)
             ){
-                Text("Detecting")
+                Text("Detecting", color=Color.White)
             }
         }
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    val dummyNavController = rememberNavController()
+    KkubeokTheme {
+        MainScreen(navController = dummyNavController)
+    }
+}
+
+
+
